d43560aea0502578d526d31281cf3b95d1f96eb4
devrcontreras <dev.rcontreras@gmail.com>
2025-03-15 15:44:36 -0400

    [add] repository for lottery loto

---
+++ src/repositories/lotoRepository.ts
@@ -0,0 +1,24 @@
+import Loto from "../models/loto.js";
+import { getConnection, mssql } from "../database/connectionSQLServer.js";
+
+export async function addLoto(loto: Loto){
+    
+    try{
+
+        const pool = await getConnection();
+
+        const result = await pool
+            .request()
+            .input("Date", mssql.NVarChar, loto.date)
+            .input("Numbers", mssql.NVarChar, loto.numbers)
+            .input("LotoMas", mssql.NVarChar, loto.lotoMas)
+            .input("SuperLotoMas", mssql.NVarChar, loto.superLotoMas)
+            .query("INSERT INTO Loto(Date, Numbers, LotoMas, SuperLotoMas) Values(@Date, @Numbers, @LotoMas, @SuperLotoMas)");
+
+        console.log(result);
+    }
+    catch(error){
+        console.error(error);
+    }
+
+}
