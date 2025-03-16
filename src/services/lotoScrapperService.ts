865a0e3724bad13ceffe0b6d6d6400cd7a6264ea
devrcontreras <dev.rcontreras@gmail.com>
2025-03-16 17:10:50 -0400

    [fix] merge the numbers into one string to store in the database

--- src/services/lotoScrapperService.ts
+++ src/services/lotoScrapperService.ts
@@ -1,6 +1,7 @@
 import * as puppeteer from 'puppeteer';
+import Loto from '../models/loto.js';
 
-export async function getLoto(year:Number) {
+export async function getLoto(year:Number): Promise<Loto[]> {
     
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
@@ -11,12 +12,12 @@ export async function getLoto(year:Number) {
 
     await page.waitForSelector('.rrm-draw-numbers');
 
-    const drawResult = await page.evaluate((currentYear) => {
+    const drawResult: Loto[] = await page.evaluate((currentYear) => {
 
         return Array.from(document.querySelectorAll('.rrm-single-result-wrap')).map(draw => ({
             
             date : `${draw.querySelector('.rrm-year-results-date').textContent.trim()} ${currentYear}`,
-            numbers : Array.from(draw.querySelectorAll('.rrm-draw-numbers span[nm]')).map(span => span.textContent.trim()),
+            numbers : Array.from(draw.querySelectorAll('.rrm-draw-numbers span[nm]')).map(span => span.textContent.trim()).join(' '),
             lotoMas : draw.querySelector('.rrm-draw-numbers span[ner]')?.textContent?.trim().replace('LOTO MAS', '') || '',
             superLotoMas : draw.querySelector('.rrm-draw-numbers span[neb]')?.textContent?.trim().replace('SUPER','').replace('LOTO', '').replace('MAS','').replace('undefined','').trim() || ''
 
