a6f78e0aa6be10474290633a783f5462355fba28
devrcontreras <dev.rcontreras@gmail.com>
2025-03-16 20:30:31 -0400

    [add] new method to get loto draws for current year

--- src/services/lotoScrapperService.ts
+++ src/services/lotoScrapperService.ts
@@ -30,3 +30,36 @@ export async function getLoto(year:Number): Promise<Loto[]> {
     return drawResult;
 
 }
+
+export async function getLotoCurrentYear(): Promise<Loto[]> {
+    
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    const url = `https://www.miresultadoloteria.com/leidsaloto/`;
+    
+    await page.goto(url, { waitUntil: 'domcontentloaded'});
+
+    await page.waitForSelector('.rrm-table-draw-date');
+
+    const year = new Date().getFullYear();
+
+    const drawResult: Loto[] = await page.evaluate((currentYear) => {
+
+        var elements = document.querySelectorAll('.rrm-draw-cntr');
+
+        return Array.from(elements).map(draw => ({
+            
+            date : `${draw.querySelector('.rrm-table-draw-date span').textContent.trim()} ${currentYear}`,
+            numbers : Array.from(draw.querySelectorAll('.rrm-upcoming-result-content span[nm]')).map(span => span.textContent.trim()).join(' '),
+            lotoMas : draw.querySelector('.rrm-upcoming-result-content span[ner]')?.textContent?.trim().replace('LOTO MAS', '') || '',
+            superLotoMas : draw.querySelector('.rrm-upcoming-result-content span[neb]')?.textContent?.trim().replace('SUPER','').replace('LOTO', '').replace('MAS','').replace('undefined','').trim() || ''
+
+        }));
+
+    }, year);
+
+    await browser.close();
+
+    return drawResult;
+}
